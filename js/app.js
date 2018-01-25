40b9f93ba45428d998e708c8c38e7ae34074337c
Vivek Maru <vivek.maru87@gmail.com>
2018-01-25 20:35:33 +1100

    Added audio and color change on Session end

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function() {
-  
+
   const toggleTimer = document.querySelector(".toggle");
   const resetTimer = document.querySelector(".reset");
   const timer = document.querySelector(".timer");
@@ -11,7 +11,8 @@
   const taskTime = taskSetting.querySelector(".time");
   const taskDec = taskSetting.querySelector(".decrease");
   const taskInc = taskSetting.querySelector(".increase");
-  
+  const audio = document.querySelector('audio');
+
   const times = {
     task: taskTime.textContent || 25,
     break: breakTime.textContent || 5,
@@ -22,14 +23,14 @@
     break: false,
     running: false,
     runID: 0,
-    
+
     init() {
       this.currTime = times.task * 60;
       timer.textContent = times.task * 60;
       taskTime.textContent = times.task;
       breakTime.textContent = times.break;
     },
-    
+
     displayTime() {
       min = Math.floor(this.currTime / 60);
     sec = this.currTime - min * 60;
@@ -38,12 +39,12 @@
       : sec}`;
     }
   };
-  
+
   function run() {
     if (state.currTime == 0) {
       state.break
         ? ((state.currTime = times.task * 60), (state.break = false))
-        : ((state.currTime = times.break * 60), (state.break = true));
+        : ((state.currTime = times.break * 60), (state.break = true), (timer.style.color = '#50B848'), (audio.play()));
     }
     state.displayTime();
     state.currTime = state.currTime - 1;
@@ -54,36 +55,36 @@
       ? (clearInterval(state.runID), (state.running = false), toggleTimer.innerHTML = 'Start')
       : ((state.runID = setInterval(run, 1000)), (state.running = true), toggleTimer.innerHTML = 'Pause');
   }
-  
+
   function changeTaskTime() {
     if(state.running) return;
-    this.textContent === '+' ? times.task += 1 : times.task < 6 ? times.task : times.task-= 1;
+    this.textContent === '+' ? times.task += 1 : times.task < 2 ? times.task : times.task-= 1;
     state.init();
     state.displayTime();
   }
-  
+
   function changeBreakTime() {
     if(state.running) return;
-    this.textContent === '+' ? times.break += 1 : times.break < 6 ? times.break : times.break-= 1;
+    this.textContent === '+' ? times.break += 1 : times.break < 2 ? times.break : times.break-= 1;
     state.init();
     state.displayTime();
   }
-  
+
   function reset() {
     times.task = 25;
     times.break = 5;
     state.init();
     state.displayTime();
   }
-  
+
   toggleTimer.addEventListener("click", toggleRunState);
   resetTimer.addEventListener("click", reset);
   taskInc.addEventListener("click", changeTaskTime);
   taskDec.addEventListener("click", changeTaskTime);
   breakInc.addEventListener("click", changeBreakTime);
   breakDec.addEventListener("click", changeBreakTime);
-  
+
   state.init();
   state.displayTime();
-  
+
 })();
